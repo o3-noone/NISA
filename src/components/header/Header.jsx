8b1ee2048ad979c914b83dd34ceedb500131d6b9
M1rzayev-web <jaloliddintuitdif315@gmail.com>
2023-06-21 15:00:28 +0500

    ok

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,7 @@ export default function Header() {
     <div className='Header'>
       <div className="container">
         <div className="navbar">
+     
         <div className='navLogo'><a href="#"><img src="/public/images/logo.svg" alt="Logo" /></a></div>
         <nav className='nav'>
             <li><a href="#">Home</a></li>
@@ -20,6 +21,20 @@ export default function Header() {
         <div className="navBurger">
         <i class="fa-solid fa-bars-staggered"></i>
         </div>
+        </div>
+        <div className="navMini">
+       <div className="navMiniExit">
+       <div className='navLogo'><a href="#"><img src="/public/images/logo.svg" alt="Logo" /></a></div>
+       <i class="fa-solid fa-xmark"></i>
+       </div>
+       <ul className="navMiniList">
+        <li><a href="#">Home</a></li>
+<li><a href="#">Pricing</a> </li>
+<li><a href="#">About</a>  </li>
+<li><a href="#">Blog</a>  </li>
+<li><a href="#">Contact</a>  </li>
+
+       </ul>
         </div>
       </div>
     </div>
